1dc7ce71cb0e79a3a10b47b9776cc36b3d777328
itaydominits <itaydominits@gmail.com>
2018-05-04 15:30:37 +0300

    first commit

---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        MySqlConnection con = new MySqlConnection(@"host=localhost;database=users;password=root;username=root");//connect to db
+        public Form2()
+        {
+            InitializeComponent();
+            con.Open();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfd = new OpenFileDialog();
+            openfd.Filter = "Images only. |*.jpg; *.jpeg; *.png; *.gif;";
+            DialogResult dr = openfd.ShowDialog();
+            if (dr==DialogResult.Cancel)
+            {
+                return;
+            }
+            button1.Image = Image.FromFile(openfd.FileName);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            MySqlCommand cmd = con.CreateCommand();//creates command from connection
+            cmd.CommandText = "insert into mainitems (mainimage,maintext) values (?image,?text)";//insert to db
+            using (MemoryStream ms = new MemoryStream())
+            {
+                button1.Image.Save(ms, ImageFormat.Bmp);
+                byte[] bytes = ms.ToArray();
+                cmd.Parameters.AddWithValue("?image",bytes );//replace image
+               
+            }
+                
+            cmd.Parameters.AddWithValue("?text",textBox1.Text);
+            cmd.ExecuteNonQuery(); 
+            
+            Addnewtextbox();
+            Addnewimage();
+        }
+        int A = 2;
+        int B = 101;
+        public System.Windows.Forms.TextBox Addnewtextbox()
+        {
+            System.Windows.Forms.TextBox txt = new System.Windows.Forms.TextBox();
+            txt.Location = new Point(120, A*109);
+            txt.Multiline = true;
+            txt.Size = new System.Drawing.Size(206, 101);
+            txt.Text = textBox1.Text.ToString();
+            this.Controls.Add(txt);
+            
+
+
+            //create a new form for every line
+            //make quantitiy
+            //make add and image buttons
+            //read headline for form 2
+
+            A++;
+            
+            if (txt.Text != string.Empty) //if not empty we need to enlarge the form)
+            {
+                this.Size = new Size(452, 556 + B);
+                B = B + 101;
+            }
+            return txt;
+            
+        }
+        int C = 2;
+        public System.Windows.Forms.PictureBox Addnewimage()
+        {
+            System.Windows.Forms.PictureBox pic = new System.Windows.Forms.PictureBox();
+            pic.Location = new Point(1,C*107);
+            pic.Size = new System.Drawing.Size(113,105);
+            pic.Image = button1.Image;
+            this.Controls.Add(pic);
+            C++;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            return pic;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button senderButton = (Button)sender;//we need to create a function to know which button was pressed. later we will take the tag according to the button that was pressed.
+
+            Form4 form4 = new Form4((int)senderButton.Tag);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            MySqlCommand cmd = con.CreateCommand();//creates command from connection
+            cmd.CommandText = "select from mainitems ;//Extract Headline";
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form3.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.rectangleShape1 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rectangleShape4 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rectangleShape3 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rectangleShape2 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.shapeContainer2 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.rectangleShape5 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(302, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label1.Location = new System.Drawing.Point(83, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sign Up";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 433);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(337, 43);
+            this.panel2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Orange;
+            this.label3.Font = new System.Drawing.Font("Arial", 20.25F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(78, 341);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 39);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Submit";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.shapeContainer1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(337, 54);
+            this.panel1.TabIndex = 7;
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.rectangleShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(337, 54);
+            this.shapeContainer1.TabIndex = 3;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // rectangleShape1
+            // 
+            this.rectangleShape1.Location = new System.Drawing.Point(85, 0);
+            this.rectangleShape1.Name = "rectangleShape1";
+            this.rectangleShape1.Size = new System.Drawing.Size(133, 9);
+            // 
+            // rectangleShape4
+            // 
+            this.rectangleShape4.BackColor = System.Drawing.Color.Orange;
+            this.rectangleShape4.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.rectangleShape4.Location = new System.Drawing.Point(14, 339);
+            this.rectangleShape4.Name = "rectangleShape4";
+            this.rectangleShape4.Size = new System.Drawing.Size(261, 45);
+            // 
+            // rectangleShape3
+            // 
+            this.rectangleShape3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.rectangleShape3.BackColor = System.Drawing.Color.White;
+            this.rectangleShape3.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.rectangleShape3.BorderColor = System.Drawing.Color.White;
+            this.rectangleShape3.CornerRadius = 3;
+            this.rectangleShape3.Location = new System.Drawing.Point(14, 138);
+            this.rectangleShape3.Name = "rectangleShape3";
+            this.rectangleShape3.Size = new System.Drawing.Size(260, 40);
+            this.rectangleShape3.Click += new System.EventHandler(this.rectangleShape2_Click);
+            // 
+            // rectangleShape2
+            // 
+            this.rectangleShape2.BackColor = System.Drawing.Color.White;
+            this.rectangleShape2.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.rectangleShape2.BorderColor = System.Drawing.Color.White;
+            this.rectangleShape2.CornerRadius = 3;
+            this.rectangleShape2.Location = new System.Drawing.Point(14, 74);
+            this.rectangleShape2.Name = "rectangleShape2";
+            this.rectangleShape2.Size = new System.Drawing.Size(260, 40);
+            this.rectangleShape2.Click += new System.EventHandler(this.rectangleShape2_Click);
+            // 
+            // shapeContainer2
+            // 
+            this.shapeContainer2.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer2.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer2.Name = "shapeContainer2";
+            this.shapeContainer2.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.rectangleShape5,
+            this.rectangleShape2,
+            this.rectangleShape3,
+            this.rectangleShape4});
+            this.shapeContainer2.Size = new System.Drawing.Size(337, 476);
+            this.shapeContainer2.TabIndex = 13;
+            this.shapeContainer2.TabStop = false;
+            // 
+            // rectangleShape5
+            // 
+            this.rectangleShape5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.rectangleShape5.BackColor = System.Drawing.Color.White;
+            this.rectangleShape5.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.rectangleShape5.BorderColor = System.Drawing.Color.White;
+            this.rectangleShape5.CornerRadius = 3;
+            this.rectangleShape5.Location = new System.Drawing.Point(15, 204);
+            this.rectangleShape5.Name = "rectangleShape5";
+            this.rectangleShape5.Size = new System.Drawing.Size(260, 40);
+            this.rectangleShape5.Click += new System.EventHandler(this.rectangleShape2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Gray;
+            this.textBox1.Location = new System.Drawing.Point(23, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Username";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Gray;
+            this.textBox2.Location = new System.Drawing.Point(25, 148);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(239, 23);
+            this.textBox2.TabIndex = 14;
+            this.textBox2.Text = "Password";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.White;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.ForeColor = System.Drawing.Color.Gray;
+            this.textBox4.Location = new System.Drawing.Point(25, 214);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(239, 23);
+            this.textBox4.TabIndex = 15;
+            this.textBox4.Text = "Email";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(337, 476);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.shapeContainer2);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape4;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape3;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape2;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer2;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
